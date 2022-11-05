563d8fd9f4eda4bbbb616540fc99487626497573
Ronald <rvpostha15@yahoo.com>
2022-11-04 22:28:57 -0400

    commit tree 62759cc95a798851c8fe2f8a02dd1e0ae4aec3df

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution in this file!
+
+var employee = {
+    name: "Sam",
+    streetAddress: "In your ass",
+};
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    var newEmployee = {...employee};
+    newEmployee[key] = '11 Broadway';
+    return newEmployee;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = `12 Broadway`;
+    return employee;
+};
+
+function deleteFromEmployeeByKey(obj, key, value) {
+    var newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value) {
+    delete employee.name;
+    return employee;
+};
+
+
+
